9c1f3d99d9a067330134cac7a27cba1881d964a9
Tamás Mojzes <tmojzi96@gmail.com>
2021-03-24 22:57:20 +0100

    Initialised Spring Boot Project and added extended .gitignore file

---
+++ src/test/java/hu/tmojzes/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.tmojzes.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
